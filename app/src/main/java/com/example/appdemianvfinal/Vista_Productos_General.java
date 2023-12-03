e9a8f2f3907e142f6e397322829e03ab5c27c934
Demian <demian.alexander.navarro@gmail.com>
2023-12-03 19:09:14 -0300

    Se agregarón las Vistas del Cliente y algunas vistas Generales

---
+++ app/src/main/java/com/example/appdemianvfinal/Vista_Productos_General.java
@@ -0,0 +1,14 @@
+package com.example.appdemianvfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vista_Productos_General extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_productos_general);
+    }
+}
